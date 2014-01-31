6498bedae9435ddcb2479c59afa3b2b22ad19e3d
Evans Mungai <evans.mungai@nokia.com>
2014-01-31 17:02:33 +0200

    Add the arduino initial code

---
+++ Arduino/main.ino
@@ -0,0 +1,33 @@
+HardwareSerial Uart = HardwareSerial();
+int vib = 15;
+
+void setup() {
+    pinMode(vib, OUTPUT);
+    Serial.begin(9600);
+    Uart.begin(115200);
+}
+
+void loop() {
+    int incomingByte;
+    if (Serial.available() > 0) {
+        incomingByte = Serial.read();
+        Serial.print("USB received: ");
+        Serial.println(incomingByte, DEC);
+        Uart.print("USB received:");
+        Uart.println(incomingByte, DEC);
+    }
+    if (Uart.available() > 0) {
+        incomingByte = Uart.read();
+        Serial.print("UART received: ");
+        Serial.println(incomingByte, DEC);
+        Uart.print("UART received:");
+        Uart.println(incomingByte, DEC);
+        analogWrite(vib, 100);
+        //digitalWrite(vib, HIGH);
+        delay(1000);
+        analogWrite(vib, 0);
+        //digitalWrite(vib, LOW);
+    }
+    delay(100);
+}
+
